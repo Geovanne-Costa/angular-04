ad8512fb51f83fe16ff058e80aa3d162049c3868
Geovanne-Costa <geovanne_al@hotmail.com>
2022-03-04 22:18:24 -0300

    2 files modified, 3 added

--- src/app/tabuada/tabuada.component.ts
+++ src/app/tabuada/tabuada.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tabuada',
   templateUrl: './tabuada.component.html',
-  styleUrls: ['./tabuada.component.css']
+  styleUrls: ['./tabuada.component.css'],
 })
 export class TabuadaComponent implements OnInit {
+  counter = 0;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  getResults() {
+    return [[0,0],
+     [1,5],
+     [2,10], 
+     [3,15], 
+     [4,20], 
+     [5,25], 
+     [6,30], 
+     [7,35], 
+     [8,40], 
+     [9,45], 
+     [10,50]];
   }
-
 }
